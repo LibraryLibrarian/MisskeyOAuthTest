9fb8722962a8c2cb7dbbd6579156541bff33bfc3
librarian <57712678+LibraryLibrarian@users.noreply.github.com>
2024-05-30 21:09:47 +0900

    Initial Commit

---
+++ MisskeyOAuthTest/MisskeyOAuthTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisskeyOAuthTestApp.swift
+//  MisskeyOAuthTest
+//
+//  Created by Librarian on 2024/05/30.
+//
+
+import SwiftUI
+
+@main
+struct MisskeyOAuthTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
